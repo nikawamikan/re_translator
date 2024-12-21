25d91fa8f5111cc44c6fc0014bdaa6f692dbb692
nikawamikan <wkwktktkkrkr@outlook.jp>
2024-12-21 15:58:34 +0000

    init: 再翻訳クソアプリつくった

---
+++ api/main.py
@@ -0,0 +1,61 @@
+from argostranslate import translate
+from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
+from functools import lru_cache
+from pydantic import BaseModel
+from typing import List
+
+app = FastAPI()
+
+
+class TranslateModel(BaseModel):
+    text: str
+    source_lang: str
+    via_langs: str
+
+
+@lru_cache(2 ** 10)
+def translate_text(text: str, source_lang: str, target_lang: str) -> str:
+    translated_text = translate.translate(
+        text,
+        source_lang,
+        target_lang
+    )
+    return translated_text
+
+
+def re_translate_text(text: str, source_lang: str, via_langs: List[str]) -> str:
+
+    # 元の言語から中間言語へ翻訳
+    translated_text = translate_text(
+        text,
+        source_lang,
+        via_langs[0]
+    )
+
+    # 中間言語から中間言語へ翻訳
+    for i in range(len(via_langs) - 1):
+        translated_text = translate_text(
+            translated_text,
+            via_langs[i],
+            via_langs[i + 1]
+        )
+
+    # 中間言語から元の言語へ翻訳
+    translated_text = translate_text(
+        translated_text,
+        via_langs[-1],
+        source_lang
+    )
+    return translated_text
+
+
+@app.get("/re_translate")
+def re_translate(text: str, source_lang: str, via_langs: str):
+    """再翻訳して人間が理解できないテキストに翻訳します。
+    """
+    return PlainTextResponse(
+        re_translate_text(
+            text,
+            source_lang,
+            via_langs.split(",")))
---
+++ bot/main.py
@@ -0,0 +1,84 @@
+from io import BytesIO
+import discord
+from discord import option
+import aiohttp
+from urllib.parse import quote
+from aiocache import cached
+
+intents = discord.Intents.default()
+
+bot = discord.Bot(intents=intents)
+
+
+@cached(600)
+async def translate_text(text: str, source_lang: str, via_langs: str) -> str:
+    encoded_text = quote(text)
+    encoded_source_lang = quote(source_lang)
+    encoded_via_langs = quote(via_langs)
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+            f"http://127.0.0.1:8000/re_translate?text={encoded_text}&source_lang={
+                encoded_source_lang}&via_langs={encoded_via_langs}"
+        ) as response:
+            translated_text = await response.text()
+
+            # なんか前後に""がついてるので削除
+            return translated_text
+
+
+@cached(600)
+async def text_to_image(text: str):
+    escaped_text = quote(text)
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+            f"https://gsapi.cbrx.io/image?top={escaped_text}&single=true"
+        ) as response:
+            image = await response.read()
+
+            return image
+
+
+@bot.slash_command(name="re_translate", description="逆翻訳します")
+@option("text", discription="翻訳するテキスト")
+@option("source_lang", discription="翻訳前の言語")
+@option("via_langs", discription="翻訳する言語 ex) en,ja")
+async def re_translate(
+    ctx: discord.ApplicationContext,
+    text: str,
+    source_lang: str,
+    via_langs: str,
+):  # Takes one integer parameter
+    await ctx.defer()
+
+    try:
+        translated_text = await translate_text(text, source_lang, via_langs)
+        await ctx.interaction.followup.send(translated_text)
+    except Exception as e:
+        await ctx.interaction.followup.send("エラーが発生しました")
+        raise e
+
+
+@bot.slash_command(name="re_translate_image", description="逆翻訳して画像にします")
+@option("text", discription="翻訳するテキスト")
+@option("source_lang", discription="翻訳前の言語")
+@option("via_langs", discription="翻訳する言語 ex) en,ja")
+async def re_translate_image(
+    ctx: discord.ApplicationContext,
+    text: str,
+    source_lang: str,
+    via_langs: str,
+):  # Takes one integer parameter
+    await ctx.defer()
+
+    try:
+        translated_text = await translate_text(text, source_lang, via_langs)
+        image = await text_to_image(translated_text)
+        byteio = BytesIO(image)
+        byteio.seek(0)
+        await ctx.interaction.followup.send(file=discord.File(byteio, "translated_image.png"))
+    except Exception as e:
+        await ctx.interaction.followup.send("エラーが発生しました")
+        raise e
+
+bot.run("YourToken")
