56ef686ef8db4fd0f0727cca955ce80471cb35c6
nikawamikan <wkwktktkkrkr@outlook.jp>
2024-12-22 03:46:37 +0000

    feat: 翻訳前後の表示をEmbedに変更

--- bot/main.py
+++ bot/main.py
@@ -54,6 +54,20 @@ async def text_to_image(text: str):
             return image
 
 
+def genelate_embed(title: str, description: str, color: discord.Colour):
+    embed = discord.Embed(title=title, description=description)
+    embed.colour = color
+    return embed
+
+
+def genelate_source_text_embed(text: str):
+    return genelate_embed("再翻訳前", text, SOURCE_EMBED_COLOR)
+
+
+def genelate_destination_text_embed(text: str):
+    return genelate_embed("再翻訳後", text, DESTINATION_EMBED_COLOR)
+
+
 @bot.slash_command(name="re_translate", description="逆翻訳します")
 @option("text", description="翻訳するテキスト")
 @option("source_lang", description="翻訳前の言語")
@@ -68,7 +82,11 @@ async def re_translate(
 
     try:
         translated_text = await translate_text(text, source_lang, via_langs)
-        await ctx.interaction.followup.send(translated_text)
+
+        source_text_embed = genelate_source_text_embed(text)
+        destination_text_embed = genelate_destination_text_embed(
+            translated_text)
+        await ctx.interaction.followup.send(embeds=[source_text_embed, destination_text_embed])
     except Exception as e:
         await ctx.interaction.followup.send("エラーが発生しました")
         raise e
@@ -91,7 +109,8 @@ async def re_translate_image(
         image = await text_to_image(translated_text)
         byteio = BytesIO(image)
         byteio.seek(0)
-        await ctx.interaction.followup.send(file=discord.File(byteio, "translated_image.png"))
+        source_text_embed = genelate_source_text_embed(text)
+        await ctx.interaction.followup.send(embed=source_text_embed, file=discord.File(byteio, "translated_image.png"))
     except Exception as e:
         await ctx.interaction.followup.send("エラーが発生しました")
         raise e
